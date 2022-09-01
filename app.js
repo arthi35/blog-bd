b3e8e46876d45ba3d478f774e77520559fec611d
arthi35 <arthimba35@gmail.com>
2022-09-01 17:40:58 +0530

    port

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ mongoose.connect(process.env.MONGO_URL, {
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
-
+  
+  app.get("/", (req, res) =>
+  res.send(`Server Running`)
+);
   app.listen(process.env.PORT || 5000, () => {
     console.log("Backend is running");
   });
